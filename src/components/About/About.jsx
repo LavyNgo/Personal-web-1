36293541ab494ae26be68aafa236e9cc2d8b909c
LavyNgo <ngothienphuc28101999@gmail.com>
2024-08-07 11:44:02 -0500

    change css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram, faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import hero from '../../assets/hero1.jpg'
-import { animal5, branson5, sky1, cas3, cas2, lameo4 } from '../../assets'
+import { animal5, branson5, sky1, cas2, branson7, lameo4 } from '../../assets'
 import React, { useState, useEffect } from 'react'
 import BackToTopBtn from '../BackToTop/BackToTopBtn'
 
@@ -18,6 +18,7 @@ const About = () => {
                 <p>
                     Welcome to my photography world! I am a passionate photographer with a love for capturing the beauty of everyday moments. Whether it's a stunning landscape, a candid portrait, or an intricate detail, I strive to tell a story through my lens.
                 </p>
+                <br />
                 <p>
                     My journey in photography began several years ago, and it has been a fulfilling adventure ever since. I believe that each photograph holds a unique narrative, and I am dedicated to sharing these stories with you.
                 </p>
@@ -29,17 +30,16 @@ const About = () => {
                     <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
                     <a href="mailto:your-email@example.com"><FontAwesomeIcon icon={faEnvelope} /></a>
                 </div>
-
-                <div className="gallery">
-                    <h2>My favorites...</h2>
-                    <div className="img-wrapper">
-                        <img src={animal5} alt="Gallery 1" />
-                        <img src={branson5} alt="Gallery 2" />
-                        <img src={sky1} alt="Gallery 3" />
-                        <img src={cas3} alt="Gallery 4" />
-                        <img src={cas2} alt="Gallery 5" />
-                        <img src={lameo4} alt="Gallery 6" />
-                    </div>
+            </div>
+            <div className="gallery">
+                <h2>My favorites...</h2>
+                <div className="img-wrapper">
+                    <img src={animal5} alt="Gallery 1" />
+                    <img src={branson5} alt="Gallery 2" />
+                    <img src={sky1} alt="Gallery 3" />
+                    <img src={branson7} alt="Gallery 4" />
+                    <img src={cas2} alt="Gallery 5" />
+                    <img src={lameo4} alt="Gallery 6" />
                 </div>
             </div>
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -17,8 +17,8 @@ const Home = () => {
             <button className="button" onClick={() => routeChange()}>View Portfolio</button>
             <div className="social-icons">
                 <a href="https://www.instagram.com" target="_blank">Instagram</a>
+                <a href="https://www.twitter.com" target="_blank">X</a>
                 <a href="https://www.facebook.com" target="_blank">Facebook</a>
-                <a href="https://www.twitter.com" target="_blank">Twitter</a>
             </div>
         </section>
     )
