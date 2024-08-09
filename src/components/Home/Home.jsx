adb8938bb0035c627b91905a5fac51a3c6c85a49
LavyNgo <ngothienphuc28101999@gmail.com>
2024-08-09 00:08:45 -0500

    change css in Home.css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,9 +16,9 @@ const Home = () => {
             <div className="subtitle">Capturing Moments, One Shot at a Time</div>
             <button className="button" onClick={() => routeChange()}>View Portfolio</button>
             <div className="social-icons">
-                <a href="https://www.instagram.com" target="_blank">Instagram</a>
-                <a href="https://www.twitter.com" target="_blank">X</a>
-                <a href="https://www.facebook.com" target="_blank">Facebook</a>
+                <a href="https://www.instagram.com" target="_blank"><p>Instagram</p></a>
+                <a href="https://www.twitter.com" target="_blank"><p>X</p></a>
+                <a href="https://www.facebook.com" target="_blank"><p>Facebook</p></a>
             </div>
         </section>
     )
